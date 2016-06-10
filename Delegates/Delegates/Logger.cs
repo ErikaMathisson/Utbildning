2ef8d9b3f3f1cbf14f0f7f7d15108387374b9f86
erika <erika@DESKTOP-88IU5OD>
2016-06-10 10:27:25 +0200

    added golfdemo and delegate

---
+++ Delegates/Delegates/Logger.cs
@@ -0,0 +1,15 @@
+﻿namespace Delegates
+{
+    internal class Logger
+    {
+        public Logger()
+        {
+        }
+
+        public void WriteMessage(string message)
+        {
+
+            System.Console.WriteLine(message);
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void Writer(string message);
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Write a message");
+            string message = Console.ReadLine();
+
+
+            Logger logger = new Logger();
+            Writer writer = new Writer(logger.WriteMessage);
+            writer(message);
+            Console.ReadKey();
+
+
+
+
+        }
+
+
+    }
+}
