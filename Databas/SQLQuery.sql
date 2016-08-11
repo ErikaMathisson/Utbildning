2d866dc2868a2e36a780c04ba987c0f5bce1a1da
ErikaMathisson <erika.mathisson@gmail.com>
2016-08-11 16:24:49 +0200

    Updated after assignment sql

---
+++ Databas/SQLQuery.sql
@@ -0,0 +1,102 @@
+﻿--INSERT INTO [Customers](
+--Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
+--VALUES(
+--'Jonas', 'Gray', '23 Green Corner Street', '56743', 'Birmingham','23 Green Corner Street', '56743', 'Birmingham', 'jonas.grey@hotmail.com', '0708123456')  
+
+--INSERT INTO [Customers](
+--Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
+--VALUES(
+--'Jane', 'Harolds', '10 West Street', '43213', 'London','10 West Street', '43213', 'London', 'jane_h77@gmail.com', '0701245512')  
+
+--INSERT INTO [Customers](
+--Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
+--VALUES(
+--'Peter', 'Birro', '12 Fox Street', '45681', 'New York','89 Moose Plaza', '45321', 'Seattle', 'peter_the_great@hotmail.com', '0739484322')  
+
+--INSERT INTO Movies(
+--Title, Director, ReleaseYear, Price)
+--VALUES(
+--'Interstellar', 'Christoper Nolan', '2014', '179'
+--)
+
+--INSERT INTO Movies(
+--Title, Director, ReleaseYear, Price)
+--VALUES(
+--'Hobbit: Battle of the five armies', 'Peter Jackson', '2014', '179'
+--)
+
+--INSERT INTO Movies(
+--Title, Director, ReleaseYear, Price)
+--VALUES(
+--'The Wolf of Wall Street', 'Martin Scorcese', '2013', '119'
+--)
+
+--INSERT INTO Movies(
+--Title, Director, ReleaseYear, Price)
+--VALUES(
+--'Pulp Fiction', 'Quentin Tarantino', '1994', '4'
+--)
+
+--Excercise 3------------------------------------------------ 
+
+--INSERT INTO Orders (OrderDate, CustomerId)
+--SELECT '2015-01-01', Customers.Id
+--FROM Customers
+--WHERE Customers.Firstname = 'Jonas' AND Customers.Lastname = 'Gray';
+
+--INSERT INTO OrderRows(OrderId, MovieId, Price)
+--SELECT o.Id, m.Id, m.Price
+--FROM Orders o, Movies m
+--WHERE o.CustomerId = 1 AND m.Title ='Interstellar'; 
+
+--INSERT INTO OrderRows(OrderId, MovieId, Price)
+--SELECT o.Id, m.Id, m.Price
+--FROM Orders o, Movies m
+--WHERE o.CustomerId = 1 AND m.Title ='Pulp Fiction';
+----------------------------------------------------------------------------
+--INSERT INTO Orders (OrderDate, CustomerId)
+--SELECT '2015-01-15', Customers.Id
+--FROM Customers
+--WHERE Customers.Firstname = 'Peter' AND Customers.Lastname = 'Birro';
+
+--INSERT INTO OrderRows(OrderId, MovieId, Price)
+--SELECT o.Id, m.Id, m.Price
+--FROM Orders o, Movies m
+--WHERE o.CustomerId = 3 AND m.Title ='The Wolf of Wall Street'; 
+
+--INSERT INTO OrderRows(OrderId, MovieId, Price)
+--SELECT o.Id, m.Id, m.Price
+--FROM Orders o, Movies m
+--WHERE o.CustomerId = 3 AND m.Title ='The Wolf of Wall Street';
+
+--------------------------------------------------------------------------------------------
+
+--INSERT INTO Orders (OrderDate, CustomerId)
+--SELECT '2014-12-20', Customers.Id
+--FROM Customers
+--WHERE Customers.Firstname = 'Jonas' AND Customers.Lastname = 'Gray';
+
+--INSERT INTO OrderRows(OrderId, MovieId, Price)
+--SELECT o.Id, m.Id, m.Price
+--FROM Orders o, Movies m
+--WHERE o.CustomerId = 1 AND m.Title ='The Wolf of Wall Street'; 
+
+----------------  Excercise 4 --------------------------------------------------
+
+--UPDATE Movies 
+--SET Price = 169
+--WHERE ReleaseYear = 2014;
+
+---------- Excercise 5 --------------------------------------------------
+
+--SELECT Firstname, Lastname, PhoneNo, EmailAddress
+--FROM Customers;
+
+--SELECT * FROM Movies ORDER BY ReleaseYear DESC;
+
+--SELECT Title
+--FROM Movies ORDER BY CAST(Price AS INT)ASC;
+
+
+
+	
---
+++ Databas/dbo.Table.sql
@@ -0,0 +1,41 @@
+﻿--DROP DATABASE [SQLDatabase];
+-- CREATE DATABASE [SQLDatabase];
+--DROP TABLE [dbo].[Customers];
+--CREATE TABLE [dbo].[Customers]
+--(
+--	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+--    [Firstname] VARCHAR(50) NOT NULL, 
+--    [Lastname] VARCHAR(50) NOT NULL, 
+--    [BillingAddress] VARCHAR(50) NOT NULL, 
+--    [BillingZip] NCHAR(10) NOT NULL, 
+--    [BillingCity] VARCHAR(50) NOT NULL,
+--    [DeliveryAddress] VARCHAR(50) NOT NULL, 
+--    [DeliveryZip] NCHAR(10) NOT NULL, 
+--    [DeliveryCity] VARCHAR(50) NOT NULL, 
+--    [EmailAddress] VARCHAR(50) NOT NULL, 
+--    [PhoneNo] NCHAR(10) NOT NULL
+--)
+
+--DROP TABLE [dbo].[Movies];
+--CREATE TABLE [dbo].[Movies](
+--[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+--[Title] VARCHAR(50) NOT NULL,
+--[Director] VARCHAR(50) NOT NULL,
+--[ReleaseYear] NCHAR(10) NOT NULL,
+--[Price] NCHAR(10) NOT NULL
+--)
+
+-- DROP TABLE [dbo].[Orders];
+--CREATE TABLE [dbo].[Orders](
+--[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+--[OrderDate] NCHAR(10) NOT NULL,
+--[CustomerId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Customers]([Id])
+--)
+
+-- DROP TABLE [dbo].[OrderRows];
+--CREATE TABLE [dbo].[OrderRows](
+--[Id] INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+--[OrderId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Orders]([Id]),
+--[MovieId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Movies]([Id]),
+--[Price] NCHAR(10) NOT NULL
+--)
