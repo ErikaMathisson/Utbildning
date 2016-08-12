87a95856f2b86c921a2d96cce6dee926214ac7ca
ErikaMathisson <erika.mathisson@gmail.com>
2016-08-12 14:56:42 +0200

    Updated sql assignment

--- Databas/SQLQuery.sql
+++ Databas/SQLQuery.sql
@@ -1,126 +1,126 @@
-﻿--INSERT INTO [Customers](
---Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
---VALUES(
---'Jonas', 'Gray', '23 Green Corner Street', '56743', 'Birmingham','23 Green Corner Street', '56743', 'Birmingham', 'jonas.grey@hotmail.com', '0708123456')  
+﻿INSERT INTO [Customers](
+Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
+VALUES(
+'Jonas', 'Gray', '23 Green Corner Street', '56743', 'Birmingham','23 Green Corner Street', '56743', 'Birmingham', 'jonas.grey@hotmail.com', '0708123456')  
 
---INSERT INTO [Customers](
---Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
---VALUES(
---'Jane', 'Harolds', '10 West Street', '43213', 'London','10 West Street', '43213', 'London', 'jane_h77@gmail.com', '0701245512')  
+INSERT INTO [Customers](
+Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
+VALUES(
+'Jane', 'Harolds', '10 West Street', '43213', 'London','10 West Street', '43213', 'London', 'jane_h77@gmail.com', '0701245512')  
 
---INSERT INTO [Customers](
---Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
---VALUES(
---'Peter', 'Birro', '12 Fox Street', '45681', 'New York','89 Moose Plaza', '45321', 'Seattle', 'peter_the_great@hotmail.com', '0739484322')  
+INSERT INTO [Customers](
+Firstname, Lastname, BillingAddress, BillingZip, BillingCity, DeliveryAddress, DeliveryZip, DeliveryCity, EmailAddress, PhoneNo)
+VALUES(
+'Peter', 'Birro', '12 Fox Street', '45681', 'New York','89 Moose Plaza', '45321', 'Seattle', 'peter_the_great@hotmail.com', '0739484322')  
 
---INSERT INTO Movies(
---Title, Director, ReleaseYear, Price)
---VALUES(
---'Interstellar', 'Christoper Nolan', '2014', '179'
---)
+INSERT INTO Movies(
+Title, Director, ReleaseYear, Price)
+VALUES(
+'Interstellar', 'Christoper Nolan', '2014', '179'
+)
 
---INSERT INTO Movies(
---Title, Director, ReleaseYear, Price)
---VALUES(
---'Hobbit: Battle of the five armies', 'Peter Jackson', '2014', '179'
---)
+INSERT INTO Movies(
+Title, Director, ReleaseYear, Price)
+VALUES(
+'Hobbit: Battle of the five armies', 'Peter Jackson', '2014', '179'
+)
 
---INSERT INTO Movies(
---Title, Director, ReleaseYear, Price)
---VALUES(
---'The Wolf of Wall Street', 'Martin Scorcese', '2013', '119'
---)
+INSERT INTO Movies(
+Title, Director, ReleaseYear, Price)
+VALUES(
+'The Wolf of Wall Street', 'Martin Scorcese', '2013', '119'
+)
 
---INSERT INTO Movies(
---Title, Director, ReleaseYear, Price)
---VALUES(
---'Pulp Fiction', 'Quentin Tarantino', '1994', '4'
---)
+INSERT INTO Movies(
+Title, Director, ReleaseYear, Price)
+VALUES(
+'Pulp Fiction', 'Quentin Tarantino', '1994', '49'
+)
 
-----Excercise 3------------------------------------------------ 
+--Excercise 3------------------------------------------------ 
 
---INSERT INTO Orders (OrderDate, CustomerId)
---SELECT '2015-01-01', Customers.Id
---FROM Customers
---WHERE Customers.Firstname = 'Jonas' AND Customers.Lastname = 'Gray';
+INSERT INTO Orders (OrderDate, CustomerId)
+SELECT '2015-01-01', Customers.Id
+FROM Customers
+WHERE Customers.Firstname = 'Jonas' AND Customers.Lastname = 'Gray';
 
---INSERT INTO OrderRows(OrderId, MovieId, Price)
---SELECT o.Id, m.Id, m.Price
---FROM Orders o, Movies m
---WHERE o.CustomerId = 1 AND m.Title ='Interstellar'; 
+INSERT INTO OrderRows(OrderId, MovieId, Price)
+SELECT o.Id, m.Id, m.Price
+FROM Orders o, Movies m
+WHERE o.CustomerId = 1 AND m.Title ='Interstellar'; 
 
---INSERT INTO OrderRows(OrderId, MovieId, Price)
---SELECT o.Id, m.Id, m.Price
---FROM Orders o, Movies m
---WHERE o.CustomerId = 1 AND m.Title ='Pulp Fiction';
-----------------------------------------------------------------------------
---INSERT INTO Orders (OrderDate, CustomerId)
---SELECT '2015-01-15', Customers.Id
---FROM Customers
---WHERE Customers.Firstname = 'Peter' AND Customers.Lastname = 'Birro';
+INSERT INTO OrderRows(OrderId, MovieId, Price)
+SELECT o.Id, m.Id, m.Price
+FROM Orders o, Movies m
+WHERE o.CustomerId = 1 AND m.Title ='Pulp Fiction';
+--------------------------------------------------------------------------
+INSERT INTO Orders (OrderDate, CustomerId)
+SELECT '2015-01-15', Customers.Id
+FROM Customers
+WHERE Customers.Firstname = 'Peter' AND Customers.Lastname = 'Birro';
 
---INSERT INTO OrderRows(OrderId, MovieId, Price)
---SELECT o.Id, m.Id, m.Price
---FROM Orders o, Movies m
---WHERE o.CustomerId = 3 AND m.Title ='The Wolf of Wall Street'; 
+INSERT INTO OrderRows(OrderId, MovieId, Price)
+SELECT o.Id, m.Id, m.Price
+FROM Orders o, Movies m
+WHERE o.CustomerId = 3 AND m.Title ='The Wolf of Wall Street'; 
 
---INSERT INTO OrderRows(OrderId, MovieId, Price)
---SELECT o.Id, m.Id, m.Price
---FROM Orders o, Movies m
---WHERE o.CustomerId = 3 AND m.Title ='The Wolf of Wall Street';
+INSERT INTO OrderRows(OrderId, MovieId, Price)
+SELECT o.Id, m.Id, m.Price
+FROM Orders o, Movies m
+WHERE o.CustomerId = 3 AND m.Title ='The Wolf of Wall Street';
 
---------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
 
---INSERT INTO Orders (OrderDate, CustomerId)
---SELECT '2014-12-20', Customers.Id
---FROM Customers
---WHERE Customers.Firstname = 'Jonas' AND Customers.Lastname = 'Gray';
+INSERT INTO Orders (OrderDate, CustomerId)
+SELECT '2014-12-20', Customers.Id
+FROM Customers
+WHERE Customers.Firstname = 'Jonas' AND Customers.Lastname = 'Gray';
 
---INSERT INTO OrderRows(OrderId, MovieId, Price)
---SELECT o.Id, m.Id, m.Price
---FROM Orders o, Movies m
---WHERE o.CustomerId = 1 AND m.Title ='The Wolf of Wall Street'; 
+INSERT INTO OrderRows(OrderId, MovieId, Price)
+SELECT o.Id, m.Id, m.Price
+FROM Orders o, Movies m
+WHERE o.CustomerId = 1 AND m.Title ='The Wolf of Wall Street'; 
 
-----------------  Excercise 4 --------------------------------------------------
+--------------  Excercise 4 --------------------------------------------------
 
---UPDATE Movies 
---SET Price = 169
---WHERE ReleaseYear = 2014;
+UPDATE Movies 
+SET Price = 169
+WHERE ReleaseYear = 2014;
 
----------- Excercise 5 --------------------------------------------------
+-------- Excercise 5 --------------------------------------------------
 
---SELECT Firstname, Lastname, PhoneNo, EmailAddress
---FROM Customers;
+SELECT Firstname, Lastname, PhoneNo, EmailAddress
+FROM Customers;
 
---SELECT * FROM Movies ORDER BY ReleaseYear DESC;
+SELECT * FROM Movies ORDER BY ReleaseYear DESC;
 
---SELECT Title
---FROM Movies ORDER BY CAST(Price AS INT)ASC;
+SELECT Title
+FROM Movies ORDER BY CAST(Price AS INT)ASC;
 
----- d. Get Firstname, Lastname, DeliveryAddress, DeliveryZip, DeliveryCity for all customers who bought The Wolf of Wall Street. 
+-- d. Get Firstname, Lastname, DeliveryAddress, DeliveryZip, DeliveryCity for all customers who bought The Wolf of Wall Street. 
 
---SELECT c.Firstname, c.Lastname, c.DeliveryAddress, c.DeliveryZip, c.DeliveryCity
---FROM Customers c, Orders o
---INNER JOIN OrderRows r ON r.MovieId = 3 AND r.OrderId = o.Id
---WHERE o.CustomerId = c.Id; 
+SELECT c.Firstname, c.Lastname, c.DeliveryAddress, c.DeliveryZip, c.DeliveryCity
+FROM Customers c, Orders o
+INNER JOIN OrderRows r ON r.MovieId = 3 AND r.OrderId = o.Id
+WHERE o.CustomerId = c.Id; 
 
-----  Exercise 6 -------------------------------
+--  Exercise 6 -------------------------------
 
----- o Add a new column, CellNo to the Customers table. The column should contain the customer’s cellphone number. 
------ (The old column, PhoneNo currently holds cellphone numbers only. ) 
+-- o Add a new column, CellNo to the Customers table. The column should contain the customer’s cellphone number. 
+--- (The old column, PhoneNo currently holds cellphone numbers only. ) 
 
---ALTER TABLE [dbo].[Customers]
--- ADD CellNo varchar(15);
+ALTER TABLE [dbo].[Customers]
+ ADD CellNo varchar(15);
 
-----  o Write a query to copy the information from PhoneNo to CellNo. 
+--  o Write a query to copy the information from PhoneNo to CellNo. 
 
---UPDATE Customers 
---SET CellNo = PhoneNo;
+UPDATE Customers 
+SET CellNo = PhoneNo;
 
----- o Write a query to empty the PhoneNo column(Sets it to an empty string) 
+-- o Write a query to empty the PhoneNo column(Sets it to an empty string) 
 
---UPDATE Customers
---SET PhoneNo = '';
+UPDATE Customers
+SET PhoneNo = '';
 
 
 
--- Databas/dbo.Table.sql
+++ Databas/dbo.Table.sql
@@ -1,43 +1,42 @@
 ﻿
--- DROP TABLE [dbo].[OrderRows];
--- DROP TABLE [dbo].[Orders];
---DROP TABLE [dbo].[Movies];
---DROP TABLE [dbo].[Customers];
---DROP DATABASE [SQLDatabase];
--- CREATE DATABASE [SQLDatabase];
+CREATE DATABASE [SQLDatabase];
+DROP TABLE [dbo].[OrderRows];
+DROP TABLE [dbo].[Orders];
+DROP TABLE [dbo].[Movies];
+DROP TABLE [dbo].[Customers];
 
---CREATE TABLE [dbo].[Customers]
---(
---	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
---    [Firstname] VARCHAR(50) NOT NULL, 
---    [Lastname] VARCHAR(50) NOT NULL, 
---    [BillingAddress] VARCHAR(50) NOT NULL, 
---    [BillingZip] NCHAR(10) NOT NULL, 
---    [BillingCity] VARCHAR(50) NOT NULL,
---    [DeliveryAddress] VARCHAR(50) NOT NULL, 
---    [DeliveryZip] NCHAR(10) NOT NULL, 
---    [DeliveryCity] VARCHAR(50) NOT NULL, 
---    [EmailAddress] VARCHAR(50) NOT NULL, 
---    [PhoneNo] NCHAR(10) NOT NULL
---)
+CREATE TABLE [dbo].[Customers]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Firstname] VARCHAR(50) NOT NULL, 
+    [Lastname] VARCHAR(50) NOT NULL, 
+    [BillingAddress] VARCHAR(50) NOT NULL, 
+    [BillingZip] NCHAR(10) NOT NULL, 
+    [BillingCity] VARCHAR(50) NOT NULL,
+    [DeliveryAddress] VARCHAR(50) NOT NULL, 
+    [DeliveryZip] NCHAR(10) NOT NULL, 
+    [DeliveryCity] VARCHAR(50) NOT NULL, 
+    [EmailAddress] VARCHAR(50) NOT NULL, 
+    [PhoneNo] NCHAR(10) NOT NULL
+)
 
---CREATE TABLE [dbo].[Movies](
---[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
---[Title] VARCHAR(50) NOT NULL,
---[Director] VARCHAR(50) NOT NULL,
---[ReleaseYear] NCHAR(10) NOT NULL,
---[Price] NCHAR(10) NOT NULL
---)
+CREATE TABLE [dbo].[Movies](
+[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+[Title] VARCHAR(50) NOT NULL,
+[Director] VARCHAR(50) NOT NULL,
+[ReleaseYear] NCHAR(10) NOT NULL,
+[Price] NCHAR(10) NOT NULL
+)
 
---CREATE TABLE [dbo].[Orders](
---[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
---[OrderDate] NCHAR(10) NOT NULL,
---[CustomerId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Customers]([Id])
---)
+CREATE TABLE [dbo].[Orders](
+[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+[OrderDate] NCHAR(10) NOT NULL,
+[CustomerId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Customers]([Id])
+)
 
---CREATE TABLE [dbo].[OrderRows](
---[Id] INT PRIMARY KEY NOT NULL IDENTITY(1,1),
---[OrderId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Orders]([Id]),
---[MovieId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Movies]([Id]),
---[Price] NCHAR(10) NOT NULL
---)
+CREATE TABLE [dbo].[OrderRows](
+[Id] INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+[OrderId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Orders]([Id]),
+[MovieId] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Movies]([Id]),
+[Price] NCHAR(10) NOT NULL
+)
